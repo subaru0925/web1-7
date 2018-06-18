2581ea551e267d51093995bbf7aa791f8cef59d8
Unknown <adgjmptw.925@icloud.com>
2018-06-18 17:36:23 +0900

    ねこ

--- CiimbCloud/Assets/PlayerController.cs
+++ CiimbCloud/Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -18,7 +19,8 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&&
+                this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
@@ -36,5 +38,14 @@ public class PlayerController : MonoBehaviour {
             transform.localScale = new Vector3(key, 1, 1);
         }
         this.animator.speed=speedx / 2.0f;
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
 	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
 }
